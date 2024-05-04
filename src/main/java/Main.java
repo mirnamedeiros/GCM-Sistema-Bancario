c78040461f85a5514ae17a3589588cd0a0c1e23e
mirnamedeiros <mirnagmedeiros@gmail.com>
2024-05-04 10:58:49 -0300

    #12 Criação do menu em loop e verificação de contas existentes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,62 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        ContaController controller = new ContaController();
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o número da conta:");
-        int numero = scanner.nextInt();
-        System.out.println(ContaController.consultarSaldo(numero));
-//        System.out.println("Conta cadastrada com sucesso!");
+        int opcao;
+
+        do {
+            System.out.println("\n--- Menu ---");
+            System.out.println("1. Criar Conta");
+            System.out.println("2. Consultar Saldo");
+            System.out.println("3. Debitar");
+            System.out.println("4. Creditar");
+            System.out.println("5. Transferir");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    boolean contaCriada = false;
+                    do {
+                        System.out.println("Digite o número da nova conta:");
+                        int numeroConta = scanner.nextInt();
+                        if (!controller.verificarContaExistente(numeroConta)) {
+                            controller.cadastrarConta(numeroConta);
+                            System.out.println("Conta criada com sucesso!");
+                            contaCriada = true;
+                        } else {
+                            System.out.println("Este número de conta já está em uso. Por favor, escolha outro número.");
+                        }
+                    } while (!contaCriada);
+                    break;
+                case 2:
+                    System.out.println("Digite o número da conta:");
+                    int numeroContaConsulta = scanner.nextInt();
+                    System.out.println(controller.consultarSaldo(numeroContaConsulta));
+                    break;
+                case 3:
+                    // Lógica para debitar
+                    break;
+                case 4:
+                    // Lógica para creditar
+                    break;
+                case 5:
+                    // Lógica para transferir
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+
+        } while (opcao != 0);
+
         scanner.close();
     }
 }
